898bb9b69c474daa5ad26aa90b9a39cba551c6e5
d <me@deetercesler.com>
2022-06-22 23:08:43 -0400

    add check for less than 1000 results

--- src/kaprekar.rs
+++ src/kaprekar.rs
@@ -1,4 +1,4 @@
-fn parse_and_iterate(loop_counter: i16, number: i16) -> i16 {
+fn parse_and_iterate(loop_counter: i32, number: i32) -> i32 {
     let int_to_str: String = number.to_string();
     let mut char_vec: Vec<char> = int_to_str.chars().collect();
     char_vec.sort(); // ascending vector
@@ -10,20 +10,33 @@ fn parse_and_iterate(loop_counter: i16, number: i16) -> i16 {
     let rev_str = reversed_vec.iter().cloned().collect::<String>();
 
     // strings to integers
-    let smaller_int: i16 = smaller_str.parse().unwrap();
-    let larger_int: i16 = rev_str.parse().unwrap();
+    let smaller_int: i32 = smaller_str.parse().unwrap();
+    let larger_int: i32 = rev_str.parse().unwrap();
 
     // find difference between the two
-    let result = larger_int - smaller_int;
+    let mut result = larger_int - smaller_int;
+
+    // make sure, if there are zeros in the number,
+    // they still get added to the end of the large number
+    // e.g. original: 1040 => small: 0014 => large: 4100
+    if result < 10 {
+        result = result * 1000
+    } else if result < 100 {
+        result = result * 100
+    } else if result < 1000 {
+        result = result * 10;
+    }
 
     if result == 6174 {
         return loop_counter + 1;
+    } else if result == 0 {
+        return -1;
     } else {
         return parse_and_iterate(loop_counter + 1, result);
     }
 }
 
-pub fn kaprekar(num: i16) -> i16 {
+pub fn kaprekar(num: i32) -> i32 {
     if num > 9999 || num < 1000 {
         return -1;
     } else if num == 6174 {
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod kaprekar;
 
 pub fn main() {
-    let answer = kaprekar::kaprekar(3321);
+    let answer = kaprekar::kaprekar(5834);
     println!("{}", answer);
 }
